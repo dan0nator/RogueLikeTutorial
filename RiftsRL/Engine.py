5c2670ac8da22f3a9fd5cb8a7f0fdc7394b11974
Dan0nator <daniel.e.mcclain@gmail.com>
2021-01-24 15:04:57 -0600

    reneamed engine

--- RiftsRL/Engine.py
+++ RiftsRL/Engine.py
@@ -7,6 +7,7 @@ from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
 
+
 class Engine:
     def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.entities = entities
