83e84fc6331d9fbb21573c41fa1a13e35f411052
dan0nator <daniel.e.mcclain@gmail.com>
2021-01-23 22:42:33 -0600

    player map

--- RiftsRL/main.py
+++ RiftsRL/main.py
@@ -6,15 +6,14 @@
 # !/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
+
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -26,6 +25,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
             screen_width,
@@ -37,25 +37,12 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
 
-            context.present(root_console)
+            engine.render(console=root_console, context=context)
 
-            root_console.clear()
+            events = tcod.event.wait()
 
-            for event in tcod.event.wait():
-
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy)
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
