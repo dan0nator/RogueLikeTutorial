d717480c73c2b07b0526adfeb8737ea2f2aaa045
Dan0nator <daniel.e.mcclain@gmail.com>
2021-01-25 15:13:21 -0600

    finished section 8

--- RiftsRL/actions.py
+++ RiftsRL/actions.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from typing import Optional, Tuple, TYPE_CHECKING
 
 import color
+import exceptions
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Actor, Entity
+    from entity import Actor, Entity, Item
 
 
 class Action:
@@ -20,20 +21,68 @@ class Action:
         return self.entity.gamemap.engine
 
     def perform(self) -> None:
-        """Perform this action with the objects needed to determine its scope
-
+        """Perform this action with the objects needed to determine its scope.
         `self.engine` is the scope this action is being performed in.
-
-        `self.entity` is the object performing the action
-
-        This method must be overridden by Action subclasses"""
-
+        `self.entity` is the object performing the action.
+        This method must be overridden by Action subclasses.
+        """
         raise NotImplementedError()
 
 
-class EscapeAction(Action):
+class PickupAction(Action):
+    """Pickup an item and add it to the inventory, if there is room for it."""
+
+    def __init__(self, entity: Actor):
+        super().__init__(entity)
+
     def perform(self) -> None:
-        raise SystemExit()
+        actor_location_x = self.entity.x
+        actor_location_y = self.entity.y
+        inventory = self.entity.inventory
+
+        for item in self.engine.game_map.items:
+            if actor_location_x == item.x and actor_location_y == item.y:
+                if len(inventory.items) >= inventory.capacity:
+                    raise exceptions.Impossible("Your inventory is full.")
+
+                self.engine.game_map.entities.remove(item)
+                item.parent = self.entity.inventory
+                inventory.items.append(item)
+
+                self.engine.message_log.add_message(f"You picked up the {item.name}!")
+                return
+
+        raise exceptions.Impossible("There is nothing here to pick up.")
+
+
+class ItemAction(Action):
+    def __init__(
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+        if not target_xy:
+            target_xy = entity.x, entity.y
+        self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """Return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+
+    def perform(self) -> None:
+        """Invoke the items ability, this action will be given to provide context."""
+        self.item.consumable.activate(self)
+
+
+class DropItem(ItemAction):
+    def perform(self) -> None:
+        self.entity.inventory.drop(self.item)
+
+
+class WaitAction(Action):
+    def perform(self) -> None:
+        pass
 
 
 class ActionWithDirection(Action):
@@ -45,18 +94,18 @@ class ActionWithDirection(Action):
 
     @property
     def dest_xy(self) -> Tuple[int, int]:
-        """Returns this actions destination"""
+        """Returns this actions destination."""
         return self.entity.x + self.dx, self.entity.y + self.dy
 
     @property
     def blocking_entity(self) -> Optional[Entity]:
-        """Return the blocking entity at this action destination.."""
+        """Return the blocking entity at this actions destination.."""
         return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
 
     @property
     def target_actor(self) -> Optional[Actor]:
-        """Return the actor at this action destination."""
-        return self.entity.gamemap.get_actor_at_location(*self.dest_xy)
+        """Return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.dest_xy)
 
     def perform(self) -> None:
         raise NotImplementedError()
@@ -66,7 +115,7 @@ class MeleeAction(ActionWithDirection):
     def perform(self) -> None:
         target = self.target_actor
         if not target:
-            return  # No entity to attack.
+            raise exceptions.Impossible("Nothing to attack.")
 
         damage = self.entity.fighter.power - target.fighter.defense
 
@@ -75,16 +124,33 @@ class MeleeAction(ActionWithDirection):
             attack_color = color.player_atk
         else:
             attack_color = color.enemy_atk
-        if damage > 0:
 
+        if damage > 0:
             self.engine.message_log.add_message(
                 f"{attack_desc} for {damage} hit points.", attack_color
             )
             target.fighter.hp -= damage
         else:
             self.engine.message_log.add_message(
-                f"{attack_desc} for {damage} hit points.", attack_color
+                f"{attack_desc} but does no damage.", attack_color
             )
+
+
+class MovementAction(ActionWithDirection):
+    def perform(self) -> None:
+        dest_x, dest_y = self.dest_xy
+
+        if not self.engine.game_map.in_bounds(dest_x, dest_y):
+            # Destination is out of bounds.
+            raise exceptions.Impossible("That way is blocked.")
+        if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            # Destination is blocked by a tile.
+            raise exceptions.Impossible("That way is blocked.")
+        if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            # Destination is blocked by an entity.
+            raise exceptions.Impossible("That way is blocked.")
+
+        self.entity.move(self.dx, self.dy)
 
 
 class BumpAction(ActionWithDirection):
@@ -94,22 +160,3 @@ class BumpAction(ActionWithDirection):
 
         else:
             return MovementAction(self.entity, self.dx, self.dy).perform()
-
-
-class MovementAction(ActionWithDirection):
-    def perform(self) -> None:
-        dest_x, dest_y = self.dest_xy
-
-        if not self.engine.game_map.in_bounds(dest_x, dest_y):
-            return  # Destination is out of bounds.
-        if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
-            return  # Destination is out of bounds.
-        if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return  # Destination is block by an entity
-
-        self.entity.move(self.dx, self.dy)
-
-
-class WaitAction(Action):
-    def perform(self) -> None:
-        pass
---
+++ RiftsRL/exceptions.py
@@ -0,0 +1,4 @@
+class Impossible(Exception):
+    """Exception raised when an action is impossible to be performed."""
+
+    """the reason is given as the exception message"""
