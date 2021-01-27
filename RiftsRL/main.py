187103df13d1e2e0951c4c12c2860e16ed8b4337
Dan0nator <daniel.e.mcclain@gmail.com>
2021-01-27 09:49:34 -0600

    start_point

--- RiftsRL/main.py
+++ RiftsRL/main.py
@@ -30,7 +30,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Inventory Tests",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
